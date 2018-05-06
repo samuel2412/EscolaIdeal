bed5346b2361cbf93294e8603a1f4ce3dfeef484
Samuel Alves <samuel241296@hotmail.com>
2018-05-05 22:58:46 -0300

    Adicionados a interface de busca os itens que constam no protipo de interface, removi o campo estado e o antigo campo de sigla. Adcionado método que seta a UF do usuario (por enquanto apenas para SP) que será usado para filtrar a busca para o estado do usuario(talvez precise recolocar a opção de selecionar o estado, mas mais pra frente vemos isso).

--- app/src/main/java/com/example/samuel/escolaideal/SearchActivity.java
+++ app/src/main/java/com/example/samuel/escolaideal/SearchActivity.java
@@ -5,10 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 
 public class SearchActivity extends Activity {
+        LinearLayout txt_help_gest;
+         private String array_spinner[],array_spinner2[];
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,20 +22,60 @@ public class SearchActivity extends Activity {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
+        txt_help_gest = (LinearLayout) findViewById(R.id.expandedMenu);
+        txt_help_gest.setVisibility(View.GONE);
+
+        array_spinner=new String[4];
+        array_spinner[0]="Municipal";
+        array_spinner[1]="Estadual";
+        array_spinner[2]="Federal";
+        array_spinner[3]="Privada";
+        Spinner s = (Spinner) findViewById(R.id.Spinner01);
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, array_spinner);
+        s.setAdapter(adapter);
+
+        array_spinner2=new String[4];
+        array_spinner2[0]="Fundamental";
+        array_spinner2[1]="Médio";
+        array_spinner2[2]="Técnico";
+        array_spinner2[3]="Superior";
+        Spinner s2 = (Spinner) findViewById(R.id.Spinner02);
+        ArrayAdapter adapter2 = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, array_spinner2);
+        s2.setAdapter(adapter2);
+
+
 
     }
 
 
     public void buscar(View view) {
 
-        EditText teste = (EditText) findViewById(R.id.siglaText);
-        String sigla = teste.getText().toString();
-         Intent i = new android.content.Intent(SearchActivity.this, ResponseActivity.class);
-
+        Intent i = new android.content.Intent(SearchActivity.this, ResponseActivity.class);
+        Bundle bundle = getIntent().getExtras();
         Bundle b = new Bundle();
-	    b.putString("key1", sigla);
+        b.putString("key1", bundle.getString("key1"));
         i.putExtras(b);
+
          startActivity(i);
+    }
+    public void itensMenu(View view){
+        Button botao = (Button) findViewById(R.id.buttonExpand);
+        /*txt_help_gest.setVisibility( txt_help_gest.isShown()
+                ? View.GONE
+                : View.VISIBLE);
+*/
+        if(txt_help_gest.isShown()){
+            txt_help_gest.setVisibility(View.GONE);
+            botao.setText("Exibir outros atributos");
+        }else{
+            txt_help_gest.setVisibility(View.VISIBLE);
+            botao.setText("Ocultar outros atributos");
+        }
+
+
+
     }
 
 }
